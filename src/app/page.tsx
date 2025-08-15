fdfc193283d4696ab0f5f6c819de11918f71b230
Abhijith Sathyendran <abhijithsathyendran@gmail.com>
2025-08-15 23:24:50 +0100

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,6 @@ export default function Home() {
     <img 
 src="https://apod.nasa.gov/apod/image/2412/LeonidsWoodcut_Vollmy_960.jpg" 
 alt="Today's Picture"
-className="w-[400px] h-auto mx-auto mt-50"/>
+className="w-[400px] h-auto mx-auto mt-50"/> 
   );
 }
