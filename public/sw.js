c268efaa5ded9488e25d54b1eff16c618b06aa6e
Abhijith Sathyendran <abhijithsathyendran@gmail.com>
2026-02-09 11:19:10 +0000

    notification_iteration_2 (#7)

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,37 @@
 self.addEventListener('push', (event) => {
     console.log('Push event received:', event);
- let title = 'New Notification';
-  let body = '';
-  if (event.data) {
-    try {
-      const data = JSON.parse(event.data.text());
-      title = data.title || title;
-      body = data.body || '';
-    } catch(e) {
-      body = event.data.text();
+    
+    let title = "Ready for today's picture of the day?";
+    let body = "New picture of the day is available. Click to view!";
+    if (event.data) { 
+        body = event.data.text();
     }
-  }
+    const options = {
+        body: body,
+        data: {
+            url: 'https://ui-server-six.vercel.app/'
+        }
+    };
   event.waitUntil(
-    self.registration.showNotification(title, { body })
+    self.registration.showNotification(title, options)
   );
+});
+
+self.addEventListener('notificationclick', (event) => {
+    console.log('Notification clicked:', event.notification.tag);
+    event.notification.close();
+    const url = event.notification.data.url;
+    event.waitUntil(
+        clients.matchAll({type: 'window', includeUncontrolled: true}).
+        then((windowClients) => {
+            for (let client of windowClients) {
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
